ea6ce6027a24645af5ecbb7a7e882ce40da75f23
Érico de Souza Loewe <ericoloewe@gmail.com>
2024-07-09 11:25:56 -0300

    router

--- gestao-trade/src/index.tsx
+++ gestao-trade/src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './pages/home/home';
+import { Home } from './pages/home/home';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
