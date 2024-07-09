a591cc4a471673048c138f0f1eefc93f649bef46
Érico de Souza Loewe <ericoloewe@gmail.com>
2024-07-09 11:01:35 -0300

    bootstrap

--- gestao-trade/src/index.tsx
+++ gestao-trade/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+import App from './pages/home/home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
