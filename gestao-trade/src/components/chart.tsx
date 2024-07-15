0b3a064d5baff4411f7091b1f9a19480b14ac1fe
Érico de Souza Loewe <ericoloewe@gmail.com>
2024-07-15 16:50:37 -0300

    chart

---
+++ gestao-trade/src/components/chart.tsx
@@ -0,0 +1,89 @@
+"use client";
+import { useEffect, useRef } from 'react';
+import { CrosshairMode, createChart } from 'lightweight-charts';
+import moment from 'moment';
+
+export interface ChartIntraday {
+  time: string,
+  open: number,
+  close: number,
+  high: number,
+  low: number,
+}
+
+interface Props {
+  data: ChartIntraday[],
+  className?: string,
+  visibleFrom?: string,
+  visibleTo?: string,
+}
+
+let chart: import('lightweight-charts').IChartApi;
+
+export const ChartComponent = (props: Props) => {
+  const { data, visibleFrom, visibleTo, ...otherProps } = props;
+
+  const chartContainerRef = useRef() as any;
+
+  useEffect(() => {
+    const handleResize = () => {
+      chart.applyOptions({ width: chartContainerRef.current.clientWidth });
+    };
+
+    chart = createChart(chartContainerRef.current, {
+      width: chartContainerRef.current.clientWidth,
+      height: 500,
+      layout: {
+        textColor: 'rgba(0, 0, 0, 0.9)',
+      },
+      grid: {
+        vertLines: {
+          color: 'rgba(197, 203, 206, 0.5)',
+        },
+        horzLines: {
+          color: 'rgba(197, 203, 206, 0.5)',
+        },
+      },
+      crosshair: {
+        mode: CrosshairMode.Normal,
+      },
+      rightPriceScale: {
+        borderColor: 'rgba(197, 203, 206, 0.8)',
+      },
+      timeScale: {
+        borderColor: 'rgba(197, 203, 206, 0.8)',
+      },
+    });
+
+    const candleSeries = chart.addCandlestickSeries({});
+
+    candleSeries.setData(data);
+
+    setVisibleRange();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+
+      chart.remove();
+    };
+  }, [data]);
+
+  useEffect(() => {
+    setVisibleRange();
+  }, [visibleFrom, visibleTo]);
+
+  function setVisibleRange() {
+    if (visibleFrom == null || visibleTo == null)
+      chart.timeScale().fitContent();
+    else // @ts-ignore
+      chart.timeScale().setVisibleRange({ from: moment(visibleFrom).unix(), to: moment(visibleTo).unix() });
+  }
+
+  return (
+    <div
+      ref={chartContainerRef}
+      {...otherProps} />
+  );
+};
--- gestao-trade/src/pages/home/home.tsx
+++ gestao-trade/src/pages/home/home.tsx
@@ -5,10 +5,14 @@ import { useEffect, useState } from 'react';
 import { Acoes } from '../../repositories/acoes';
 import { useStorage } from '../../contexts/storage';
 import { AcaoCard } from '../acoes/acao-card/acao-card';
+import { ChartComponent } from '../../components/chart';
+
+interface HistoricoAcao { }
 
 export function Home() {
-  const [stocks, setStocks] = useState<Acoes[]>();
-  const [currentStock, setCurrentStock] = useState<string>();
+  const [acoes, setAcoes] = useState<Acoes[]>();
+  const [acaoEscolhida, setAcaoEscolhida] = useState<string>();
+  const [historico, setHistorico] = useState<HistoricoAcao[]>([]);
   const { isDbOk, repository } = useStorage();
 
   useEffect(() => {
@@ -23,16 +27,16 @@ export function Home() {
   useEffect(() => {
     if (isDbOk)
       loadHistory();
-  }, [isDbOk, currentStock]);
+  }, [isDbOk, acaoEscolhida]);
 
   async function load() {
     const acoes = await repository.acoes.listActives();
 
-    setStocks(acoes);
+    setAcoes(acoes);
   }
 
   async function loadHistory() {
-    if (currentStock == null) return;
+    if (acaoEscolhida == null) return;
 
     // const response = await fetch(`https://localhost:7062/stock/history?stockId=${currentStock}`);
     // const json = (await response.json()) as StockHistoryResponse;
@@ -51,16 +55,25 @@ export function Home() {
   return (
     <Layout>
       <section className='container'>
-        {stocks == null ? <div className="alert alert-info">Carregando ações ativas...</div>
+        {acoes == null ? <div className="alert alert-info">Carregando ações ativas...</div>
           : (
-            stocks.length === 0 ? (<div className="alert alert-info">Nenhuma ação ativa...</div>) : (
+            acoes.length === 0 ? (<div className="alert alert-info">Nenhuma ação ativa...</div>) : (
               <div className="stocks">
-                {stocks.map(x => (
-                  <AcaoCard key={x.codigo} acao={x} isActive={x.codigo === currentStock} onClick={s => setCurrentStock(s.codigo)} />
+                {acoes.map(x => (
+                  <AcaoCard key={x.codigo} acao={x} isActive={x.codigo === acaoEscolhida} onClick={s => setAcaoEscolhida(s.codigo)} />
                 ))}
               </div>
             )
           )
+        }
+        {acaoEscolhida == null ? <div className="alert alert-warning">Escolha uma ação para analisar</div>
+          : <>
+            <section className="card analysis">
+              <div className="card-header">Analise</div>
+            </section>
+            {historico == null ? <div className="alert alert-info">Carregando histórico...</div>
+              : <ChartComponent className="chart container-sm" data={[]}></ChartComponent>}
+          </>
         }
       </section>
     </Layout>
